ee823747e765e2288a24bcf66c69c6c0616578b7
dhuangcodes <dhuang3@andrew.cmu.edu>
2026-04-18 01:19:21 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DISCORD_WEBHOOK_URL  = os.getenv("DISCORD_WEBHOOK_URL", "")
+BOT_TOKEN            = os.getenv("BOT_TOKEN", "")
 MIN_TRADE_USD        = float(os.getenv("MIN_TRADE_USD", "1000"))
-POLL_INTERVAL        = int(os.getenv("POLL_INTERVAL", "30"))
-TOP_WALLETS_COUNT    = int(os.getenv("TOP_WALLETS_COUNT", "300"))  # top wallets to monitor
+POLL_INTERVAL        = int(os.getenv("POLL_INTERVAL", "45"))
+TOP_WALLETS_COUNT    = int(os.getenv("TOP_WALLETS_COUNT", "300"))
